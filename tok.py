e0ead09e67db8f2d07eab192689f0984990300e8
Andrei Popescu <andreigabrielpopescu556@gmail.com>
2023-07-16 23:21:18 +0300

    Removed tok file

--- tok.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if __name__ == '__main__':
-    print(torch.cuda.is_available())
-    print(torch.cuda.device_count())
-    print(torch.cuda.get_device_name(0))
-    # device memory
-    print(torch.cuda.memory_allocated())
-    
